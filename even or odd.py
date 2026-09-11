cb6c3f47122353aafdbbc15c402fcc0fed23b1fe
Shivamani <masutimani8@gmail.com>
2026-09-11 15:52:20 +0530

    updated the even or odd

--- even or odd.py
+++ even or odd.py
@@ -2,5 +2,7 @@ num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print("The number is Even")
+    print("The number is prime")
 else:
     print("The number is Odd")
+    print("The number is not prime")
